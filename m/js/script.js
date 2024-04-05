efc35792c45cd7deffd57f641ddd02c127710f70
lpg-etec <luiggy.garcia@etec.sp.gov.br>
2024-04-05 11:29:40 -0300

    m

---
+++ m/js/script.js
@@ -0,0 +1,19 @@
+document.getElementById('black').addEventListener('click', () => {
+        tyle.backgroundColor= 'black'
+    t.style.color = 'white'
+})
+
+document.getElementById('white').addEventListener('click', () => {
+    t.style.backgroundColor = 'white'
+    t.style.color = 'black'
+})
+
+document.getElementById('red').addEventListener('click', () => {
+    t.style.backgroundColor = 'red'
+    t.style.color = 'white'
+})
+
+document.getElementById('blue').addEventListener('click', () => {
+    t.style.backgroundColor = 'blue'
+    t.style.color = 'white'
+})
