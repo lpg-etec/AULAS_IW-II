c8b9f9758448f1e8730941804c8969cbb8396594
lpg-etec <luiggy.garcia@etec.sp.gov.br>
2024-08-01 09:00:49 -0300

    react

---
+++ b2/react-app/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import styles from './Header.module.css';
+
+function Header() {
+  return (
+    <header className={styles.header}>
+        <span>EtecFlix</span>
+    </header>
+  );
+}
+
+export default Header;
