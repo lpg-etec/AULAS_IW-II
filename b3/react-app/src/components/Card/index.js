ad69866971413c587e9caa2e3762c262522f1d0c
lpg-etec <luiggy.garcia@etec.sp.gov.br>
2024-09-26 08:13:57 -0300

    a7-20

--- b3/react-app/src/components/Card/index.js
+++ b3/react-app/src/components/Card/index.js
@@ -1,14 +1,30 @@
+import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
+import iconFavorite from "./favorite.png";
+import iconUnfavorite from "./unfavorite.png";
+import { useFavoriteContext } from "../../contexts/Favorites";
 
 function Card({ id }) {
+    const { favorite, addFavorite } = useFavoriteContext()
+    const isFavorite = favorite.some((fav) => fav.id === id)
+	const icone = !isFavorite ? iconFavorite : iconUnfavorite
+
     return (
         <section className={styles.card}>
-            <a
-                href={`https://www.youtube.com/watch?v=${id}`} 
-                rel="noreferrer noopener"
-                target="_blank">
-                <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`} alt="Capa" />
-            </a>
+            <Link to={`/watch/${id}`} >
+                <img
+                    src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`}
+                    alt="Capa"
+                    className={styles.capa}
+                />
+                </Link>
+                <figure className={styles.icon}>
+                    <img
+                        src={icone}
+                        alt="Ícone"
+                        onClick={() => addFavorite({id})}
+                    />
+                </figure>
         </section>
     );
 }
